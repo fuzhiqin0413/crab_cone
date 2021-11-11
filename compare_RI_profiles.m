bf691c046991fb0ae5b31f22bf9fea943a3c4dab
Gavin Taylor <gavin.taylor.01@gmail.com>
2021-11-11 13:17:44 -0200

    ADDED CYLINDER AND VOLUME
    Looks pretty good!

--- compare_RI_profiles.m
+++ compare_RI_profiles.m
@@ -1,10 +1,13 @@
 meanConeL = 470;
+expConeL = 600;
 
 n0 = 1.52;
 
 radius = 0:100;
 
-RIConeEqn = n0*sech(pi*radius/2/radius);
+RIConeEqn_Mean = n0*sech(pi*radius/2/meanConeL);
+
+RIConeEqn_Exp = n0*sech(pi*radius/2/expConeL);
 
 RIOrignal = 1.52-0.000004914*radius.^2;
 
@@ -12,7 +15,11 @@ RIEndLongitudional = 1.5+(0.02)-0.00000612*radius.^2;
 
 figure; hold on
 
-plot(radius, RIConeEqn);
-plot(radius, RIOrignal);
-plot(radius, RIEndLongitudional);
+h1 = plot(radius, RIConeEqn_Mean);
+h2 = plot(radius, RIConeEqn_Exp);
+h3 = plot(radius, RIOrignal);
+h4 = plot(radius, RIEndLongitudional);
 ylim([1.35 1.55])
+
+legend([h1 h2 h3 h4], 'Lens Cylinder Eqn - 470', 'Lens Cylinder Eqn - 600',...
+    'Original radial graident', 'Longitudional at tip + radial gradient')
--- create_2d_cone_slice.m
+++ create_2d_cone_slice.m
@@ -4,21 +4,40 @@ get_radius_profiles
 %  close all
 voxSize = 2.18;
 
-scaleUpVoxels = 0;
-    newVoxSize = 0.5;
+scaleUpVoxels = 1;
+    newVoxSize = 2;
     voxScale = voxSize/newVoxSize; % Scale up factor on image for FDTD
-
+    
 writeImage = 0;
-    fileName = '500_average_cone_0_sd.csv';
-
-displayRI = 1;
+    fileNameBase = 'Cone';
+    
+create3D = 1;
+    % Seems a bit smoother to scale up here than just set desired size in image
+        % E.g. To get voxSize 2, a bit better to have newVoxSize as 1 and resize3DRatio 2 
+        % than newVoxSize as 2 directly because of radial interpolant    
+    resize3DRatio = 1; % scales up from image, should be integer > 1
+    write3D = 0;
     
 % For display
-nPlot = 4;
+plotMulti = 0;
+nPlot = 5;
 tText = 'Smooth - 2.18 um'; %'-2 SD'; 'Mean';
+% mainFig = figure;
 
-SDMult = 0;
-mainFig = figure;
+makeLabels = 0;
+
+% Set SD mult for cones and distances
+SDMult.ConeLength = 0;
+SDMult.OuterCorneaLength = 0;
+SDMult.EpiCorneaLength = 0;
+SDMult.ConeProfile = 0;
+SDMult.CinCProfile = 0;
+SDMult.ExposedInterconeProfile = 0;
+SDMult.InternalInterconeProfile = 0;
+SDMult.EpicorneaProfile = 0;
+
+displayProfiles.CinC = 0;
+displayProfiles.EpicorneaCone = 0;
 
 %Can be from 1 - 4, can also be equal
 interconeOnLeft = NaN;
@@ -27,26 +46,29 @@ interconeOnRight = NaN;
 tipOffset = 30; 
 bufferLength = 10;
  
-reduceTipsCone = 1;
-reduceTipsCinC = 1;
-reduceTipsCornea = 1;
+reduceTips.Cone = 1;
+reduceTips.CinC = 1;
+reduceTips.Cornea = 1;
 
 smoothProfiles = 1;
     smoothLength = 5;
     smoothLengthIntercone = 20;
 
 tipGradientCorrection = 0;    
-    correctionType = 'both'; %'distance', 'height', 'both'
-    correctionScale = 1;
+    correctionType = 'distance'; %'distance', 'height', 'both'
+    correctionScale = 2;
     
-if displayRI
+createCylinder = 0; % instead of a cone   
+    cylinderRILength = 600; % if nan it uses actual length measured (~470) should be in um
+    % if createCylinder set then cone value should usually also be set to 'cylinder'
+
+if ~makeLabels
     outerValue = 1.33;
     innerValue = 1.34;
-    coneValue = 'radial'; %'radial', 'linear' 'both'
+    coneValue = 'radial'; %'cylinder', 'radial', 'linear' 'both'
     outerCorneaValue = 1.5;
     epicorneaValue = 1.53;
-    interconeValue = 1.47;
-
+    interconeValue = -1; 1.47;
 else
 % Labels
     outerValue = 0;
@@ -57,7 +79,7 @@ else
     interconeValue = 2;
 end
 
-if reduceTipsCone
+if reduceTips.Cone
     bufferOffset = 9;
 else
     bufferOffset = 10;
@@ -67,17 +89,25 @@ if bufferOffset > bufferLength
     error('buffer offset to large')
 end
 
-coneLengthToUse = (bufferLength - bufferOffset) + mean(lengthsToPlanes(:,2)) + std(lengthsToPlanes(:,2))*SDMult;
+coneLengthToUse = (bufferLength - bufferOffset) + mean(lengthsToPlanes(:,2)) + std(lengthsToPlanes(:,2))*SDMult.ConeLength;
 
-% interconeLengthToUse = (bufferLength - bufferOffset) + mean(lengthsToPlanes(:,1)) + std(lengthsToPlanes(:,1))*SDMult;
+outerCorneaLengthToUse = (bufferLength - bufferOffset) + mean(lengthsToPlanes(:,3)) + std(lengthsToPlanes(:,3))*SDMult.OuterCorneaLength - coneLengthToUse;
 
-outerCorneaLengthToUse = (bufferLength - bufferOffset) + mean(lengthsToPlanes(:,3)) + std(lengthsToPlanes(:,3))*SDMult - coneLengthToUse;
-
-epicorneaLengthToUse = (bufferLength - bufferOffset) + mean(lengthsToPlanes(:,4)) + std(lengthsToPlanes(:,4))*SDMult - coneLengthToUse - outerCorneaLengthToUse;
+epicorneaLengthToUse = (bufferLength - bufferOffset) + mean(lengthsToPlanes(:,4)) + std(lengthsToPlanes(:,4))*SDMult.EpiCorneaLength - coneLengthToUse - outerCorneaLengthToUse;
 
 restretchLength_cone = ceil(max(lengthsToPlanes(:,2))/100)*100;
 
 restretchLength_cornea = ceil((max(lengthsToPlanes(:,4))-min(lengthsToPlanes(:,3)))/10)*10;
+
+% Assign cylinder length and check some related points
+if isnan(cylinderRILength)
+   cylinderRILength = coneLengthToUse*voxSize;
+end
+
+if createCylinder & ~strcmp('cylinder', coneValue)
+   warning('Making a cylinder but not set to use lens cylinder profile') 
+end
+
 
 [meanStretchedCone, stdStretchedCone, coneXRef] = restretchProfile(coneAverage(:,bufferOffset+1:end), numCones, depthTests(bufferLength+1:end), ...
         coneRefDiameter, ones(numCones,1), ceil(lengthsToPlanes(:,2)), mean(coneRefDiameter), coneLengthToUse, restretchLength_cone, 0, 0, 0);
@@ -111,7 +141,10 @@ subplot(1,3,1); hold on
 errorbar(coneXRef, meanStretchedCone*voxSize, stdStretchedCone*voxSize);
 errorbar(coneXRef, meanStretchedCinC*voxSize, stdStretchedCinC*voxSize);
 
-if reduceTipsCone
+firstInd = find(~isnan(meanStretchedCone)); firstInd = firstInd(1);
+initialConcentratorAcceptance = asin(sqrt(1/(meanStretchedCone(end)/meanStretchedCone(firstInd))))/pi*180
+
+if reduceTips.Cone
     %%% Tried interpolation but didn't work well
     % decided to add a step with half radius at top so it's not too blunt
 
@@ -124,7 +157,7 @@ if reduceTipsCone
     plot(coneXRef(goodConeInds(1)-1), meanStretchedCone(goodConeInds(1)-1)*voxSize, 'mx');
 end
 
-if reduceTipsCinC
+if reduceTips.CinC
     % Reduce CinC tip
     goodCinCInds = find(~isnan(meanStretchedCinC));
     
@@ -142,7 +175,7 @@ errorbar(coneXRef, meanStretchedInternalInterconeRight*voxSize, stdStretchedInte
 subplot(1,3,3); hold on
 errorbar(corneaXRef, meanStretchedEpicorneaInner*voxSize, stdStretchedEpicorneaInner*voxSize);
 
-if reduceTipsCornea
+if reduceTips.Cornea
     % reduce epicornea cone tip
     goodEpicorneaInds = find(~isnan(meanStretchedEpicorneaInner));
     
@@ -153,22 +186,22 @@ if reduceTipsCornea
 end
 
 % Get profiles to use
-coneProfileToUse =  meanStretchedCone + stdStretchedCone*SDMult;
+coneProfileToUse =  meanStretchedCone + stdStretchedCone*SDMult.ConeProfile;
 coneProfileToUse(coneProfileToUse < 0) = NaN;
 
-CinCProfileToUse =  meanStretchedCinC + stdStretchedCinC*SDMult;
+CinCProfileToUse =  meanStretchedCinC + stdStretchedCinC*SDMult.CinCProfile;
 CinCProfileToUse(CinCProfileToUse < 0) = NaN;
 
-exposedInterconeProfileToUse = meanStretchedExposedIntercone + stdStretchedExposedIntercone*SDMult;
+exposedInterconeProfileToUse = meanStretchedExposedIntercone + stdStretchedExposedIntercone*SDMult.ExposedInterconeProfile;
 exposedInterconeProfileToUse(exposedInterconeProfileToUse < 0) = 0;
 
-internalInterconeProfileToUseLeft = meanStretchedInternalInterconeLeft + stdStretchedInternalInterconeLeft*SDMult;
+internalInterconeProfileToUseLeft = meanStretchedInternalInterconeLeft + stdStretchedInternalInterconeLeft*SDMult.InternalInterconeProfile;
 internalInterconeProfileToUseLeft(internalInterconeProfileToUseLeft < 0) = 0;
 
-internalInterconeProfileToUseRight = meanStretchedInternalInterconeRight + stdStretchedInternalInterconeRight*SDMult;
+internalInterconeProfileToUseRight = meanStretchedInternalInterconeRight + stdStretchedInternalInterconeRight*SDMult.InternalInterconeProfile;
 internalInterconeProfileToUseRight(internalInterconeProfileToUseRight < 0) = 0;
 
-epicorneaProfileToUse = meanStretchedEpicorneaInner + stdStretchedEpicorneaInner*SDMult;
+epicorneaProfileToUse = meanStretchedEpicorneaInner + stdStretchedEpicorneaInner*SDMult.EpicorneaProfile;
 epicorneaProfileToUse(epicorneaProfileToUse < 0) = NaN;
 
 % Clip top of exposed cone on both sides
@@ -252,6 +285,7 @@ if scaleUpVoxels
     epicorneaProfileToUse = interp1(corneaXRef, epicorneaProfileToUse, tempCorenaXRef,'linear')*voxScale;
     
     voxSize = newVoxSize;
+    voxSize3D = voxSize*resize3DRatio;
     
     coneXRef = tempConeXRef;
     corneaXRef = tempCorenaXRef;
@@ -260,6 +294,11 @@ end
 subplot(1,3,1); hold on
 plot(coneXRef, coneProfileToUse*voxSize);
 plot(coneXRef, CinCProfileToUse*voxSize);
+
+firstInd = find(~isnan(coneProfileToUse)); firstInd = firstInd(1);
+line([coneXRef(firstInd) coneXRef(end)], [-coneProfileToUse(firstInd) coneProfileToUse(end)]*voxSize);
+
+concentratorAcceptance = asin(sqrt(1/(coneProfileToUse(end)/coneProfileToUse(firstInd))))/pi*180
 
 subplot(1,3,2); hold on
 plot(coneXRef, exposedInterconeProfileToUseLeft*voxSize);
@@ -308,8 +347,15 @@ for i = 1:sliceSize(2)
                coneProfileToUse(i-tipOffset) = coneProfileToUse(i-tipOffset-1); 
             end
 
-            xPosTop = round(sliceSize(1)/2 + coneProfileToUse(i-tipOffset));
-            xPosBottom = round(sliceSize(1)/2 - coneProfileToUse(i-tipOffset));
+            if ~createCylinder
+                % create cone
+                xPosTop = round(sliceSize(1)/2 + coneProfileToUse(i-tipOffset));
+                xPosBottom = round(sliceSize(1)/2 - coneProfileToUse(i-tipOffset));
+            else
+                % create cylinder using max value
+                xPosTop = round(sliceSize(1)/2 + max(coneProfileToUse));
+                xPosBottom = round(sliceSize(1)/2 - max(coneProfileToUse));
+            end
                         
             distMap(xPosTop+1:end, i) = 1;
             distMap(1:xPosBottom-1, i) = 1;
@@ -337,9 +383,16 @@ for i = 1:sliceSize(2)
         end
 
         if passedConeTip
-            xPosTop = round(sliceSize(1)/2 + coneProfileToUse(i-tipOffset));
-            xPosBottom = round(sliceSize(1)/2 - coneProfileToUse(i-tipOffset));
-        
+            if ~createCylinder
+                % create cone
+                xPosTop = round(sliceSize(1)/2 + coneProfileToUse(i-tipOffset));
+                xPosBottom = round(sliceSize(1)/2 - coneProfileToUse(i-tipOffset));
+            else
+                % create cylinder using max value
+                xPosTop = round(sliceSize(1)/2 + max(coneProfileToUse));
+                xPosBottom = round(sliceSize(1)/2 - max(coneProfileToUse));
+            end
+            
             % Fill between top and bottom
             if isnumeric(coneValue)
                 slice(xPosBottom:xPosTop, i) = coneValue;
@@ -347,11 +400,10 @@ for i = 1:sliceSize(2)
                 
                 tempRadius = abs((xPosBottom:xPosTop)-sliceSize(1)/2);
                 
+                fullRad = mean(abs(tempRadius([1 end])));
+                
                 if tipGradientCorrection
                     tempDist = distMap(xPosBottom:xPosTop,i);
-
-                    warning('Final layer seems too low relative to rest of border')
-                    %%% Maybe remove the -1?
                     
                     switch correctionType
                         case 'distance'
@@ -369,25 +421,30 @@ for i = 1:sliceSize(2)
                     
                 end
                 
-                % rescale relative diameter to be 80
-                tempRadius = tempRadius/coneProfileToUse(i-tipOffset)*80;
-                
                 % rescale length relative to full cone
                 tempZ = (coneLengthToUse-(i-tipOffset))/coneLengthToUse;
                 
                 switch coneValue
+                    case 'cylinder'
+                        % note this radius is not rescaled and will produce lower values than radial at radius > |80| 
+                        tempRI = 1.52*sech(pi*tempRadius*voxSize/2/cylinderRILength);
+                        
                     case 'radial'
                         % Original
-                        error('switch back to original profile')
-%                         tempRI = 1.52-0.000004914*tempRadius.^2;
+                        % rescale relative diameter to 80
+                        tempRadius = tempRadius/fullRad*80;
+                                        
+                        tempRI = 1.52-0.000004914*tempRadius.^2;
                         
                         % For for linear contribution
-                        tempRI = 1.5+(0.02)-0.00000612*tempRadius.^2;
+%                       tempRI = 1.5+(0.02)-0.00000612*tempRadius.^2;
                         
                     case 'linear'
                         tempRI = 1.5+(tempZ*0.01+0.01);
                         
                     case 'both'
+                        tempRadius = tempRadius/fullRad*80;
+                        
                         tempRI = 1.5+(tempZ*0.01+0.01)-0.00000612*tempRadius.^2;
                         
                 end
@@ -402,6 +459,11 @@ for i = 1:sliceSize(2)
             slice(1:xPosBottom-1, i) = innerValue;
         end
 
+        if ~isnan(exposedInterconeProfileToUseLeft(i-tipOffset)) |  ~isnan(exposedInterconeProfileToUseRight(i-tipOffset))
+                endTipWidth = coneProfileToUse(i-tipOffset);
+                interconeEndInd = i;
+        end
+        
         % Place exposed cone and other cone profile.
         %left
         if ~isnan(exposedInterconeProfileToUseLeft(i-tipOffset))
@@ -477,7 +539,7 @@ for i = 1:sliceSize(2)
         end
 
         % Place cone in cone profile
-        if ~isnan(CinCProfileToUse(i-tipOffset))
+        if ~isnan(CinCProfileToUse(i-tipOffset)) & displayProfiles.CinC
             xPosCinCTop = round(sliceSize(1)/2 + CinCProfileToUse(i-tipOffset));
             xPosCinCBottom = round(sliceSize(1)/2 - CinCProfileToUse(i-tipOffset));
 
@@ -498,7 +560,7 @@ for i = 1:sliceSize(2)
         
         slice(:, i) = epicorneaValue;
 
-        if ~isnan(epicorneaProfileToUse(i-round(coneLengthToUse + tipOffset + outerCorneaLengthToUse)))
+        if ~isnan(epicorneaProfileToUse(i-round(coneLengthToUse + tipOffset + outerCorneaLengthToUse))) & displayProfiles.EpicorneaCone
             xPosCorneaTop = round(sliceSize(1)/2 + epicorneaProfileToUse(i-round(coneLengthToUse + tipOffset + outerCorneaLengthToUse)));
             xPosCorneaBottom = round(sliceSize(1)/2 - epicorneaProfileToUse(i-round(coneLengthToUse + tipOffset + outerCorneaLengthToUse)));
 
@@ -507,24 +569,111 @@ for i = 1:sliceSize(2)
     end
 end
 
-figure(mainFig);
+figure; 
+subplot(1,2,1); imshow((slice'-1.45)/(1.54-1.45))
 
-subplot(2,3,nPlot)
-if ~ischar(coneValue)
-    imshow(slice'/~(max(slice(:))+1))
-else
-    % For tips
-%     imshow((slice(40:100,20:80)'-1.45)/(1.54-1.45))
+tempRange = round(sliceSize(1)/2-endTipWidth*1.5):round(sliceSize(1)/2+endTipWidth*1.5);
+sliceTip = slice(tempRange, 1:(interconeEndInd+round(5/voxSize)));
+subplot(1,2,2); imshow((sliceTip'-1.45)/(1.54-1.45))
+
+if plotMulti
+    figure(mainFig);
+
+    subplot(2,3,nPlot)
+    if makeLabels
+        imshow(slice'/~(max(slice(:))+1))
+    else
+
+        imshow((slice'-1.45)/(1.54-1.45))
     
-    imshow((slice'-1.45)/(1.54-1.45))
+        title(tText);
+    end
 end
 
-title(tText);
+currentDirectory = pwd; 
+
+if writeImage | write3D
+    if ischar(coneValue)
+        fileName = sprintf('%s_%i_nm_%i_SD_GRIN',fileNameBase, round(voxSize*1000), SDMult.ConeProfile);
+    else
+        fileName = sprintf('%s_%i_nm_%i_SD_Uniform',fileNameBase, round(voxSize*1000), SDMult.ConeProfile);
+    end
+    
+    cd('/Users/gavintaylor/Desktop/AnalysisImages')
+    
+    save(fileName, 'voxSize', 'voxSize3D', 'resize3DRatio', 'SDMult', 'displayProfiles', 'tipOffset', 'bufferLength', 'reduceTips', ...
+        'smoothProfiles', 'smoothLength', 'smoothLengthIntercone', 'tipGradientCorrection', 'correctionType', 'correctionScale', ...
+        'outerValue', 'innerValue', 'coneValue', 'outerCorneaValue', 'epicorneaValue', 'interconeValue', ...
+        'coneLengthToUse', 'outerCorneaLengthToUse', 'epicorneaLengthToUse', ...
+        'coneProfileToUse', 'CinCProfileToUse', 'exposedInterconeProfileToUseLeft', 'exposedInterconeProfileToUseRight', 'internalInterconeProfileToUseLeft', 'internalInterconeProfileToUseRight', 'epicorneaProfileToUse', ...
+        'coneXRef', 'corneaXRef', 'concentratorAcceptance', 'sliceSize', ...
+        'createCylinder', 'cylinderRILength')
+end
 
 if writeImage
-    currentDirectory = pwd; 
-    cd('/Users/gavintaylor/Desktop')
-
-    writematrix(slice,fileName) 
-    cd(pwd)
+    writematrix(slice,sprintf('%s.csv', fileName)) 
+    
+    writematrix(sliceTip,sprintf('Tip_%s.csv', fileName)) 
 end
+
+if create3D
+    
+   if rem(resize3DRatio,1)
+      error('Ratio will probably work best as an integer multiple') 
+   end
+   
+   if resize3DRatio < 1
+      error('Ratio should downsize') 
+   end
+   
+   % Note that this starts of as anisotropic 
+   volumeSize = [round(sliceSize(1)/resize3DRatio), round(sliceSize(1)/resize3DRatio), sliceSize(2)]; 
+   volume = zeros(volumeSize);
+   
+   % set up to rotate around center
+   [volX, volY, volZ] = meshgrid(1:volumeSize(1), 1:volumeSize(2), 1:volumeSize(3));
+   volX = volX(:) - volumeSize(1)/2;
+   volY = volY(:) - volumeSize(2)/2;
+   volZ = volZ(:);
+   
+   volRadius = sqrt(volX.^2 + volY.^2)*resize3DRatio;
+   
+   [sliceZ, sliceRadius] = meshgrid(1:sliceSize(2), 1:sliceSize(1));
+   sliceRadius = sliceRadius(:) - sliceSize(1)/2;
+   sliceZ = sliceZ(:);
+   
+   %step down each slice and interpolate based on radius
+   for i = 1:sliceSize(2)
+      volInds = find(volZ == i);
+      
+      sliceInds = find(sliceZ == i & sliceRadius >= 0);
+      
+      % folds left side back... not ideal
+      volume(volInds) = interp1(sliceRadius(sliceInds), slice(sliceInds), volRadius(volInds), ...
+        'nearest', 'extrap');
+   end
+   
+   % Removes anistropy by compressing Z down
+   if resize3DRatio > 1
+        volume = imresize3(volume, 'Scale', [1 1 1/resize3DRatio], 'method', 'nearest');
+   end
+   
+   figure;
+   subplot(2,2,1);
+   imshow((permute(volume(:,round(volumeSize(1)/2),:), [1 3 2])-1.45)'/(1.54-1.45))
+   
+   subplot(2,2,2);
+   imshow((permute(volume(round(volumeSize(2)/2),:,:), [2 3 1])-1.45)'/(1.54-1.45))
+   
+   subplot(2,2,3);
+   imshow((volume(:,:,round(volumeSize(3)/4/resize3DRatio))-1.45)/(1.54-1.45))
+   
+   subplot(2,2,4);
+   imshow((volume(:,:,round(volumeSize(3)/2/resize3DRatio))-1.45)/(1.54-1.45))
+   
+   if write3D
+       writematrix(volume,sprintf('Volume_%s.csv', fileName)) 
+   end
+end
+
+cd(pwd)
